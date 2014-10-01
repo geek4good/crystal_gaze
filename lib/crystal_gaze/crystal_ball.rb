cfb5c0b77344d67ad8ba8da2166862e222eff8a7
Lucas Mbiwe <lucas@geek4good.com>
2014-10-01 23:20:11 +0200

    Enter the spirit world for visions
    * make predictions using visions
    * enter the spirit world to get these visions

--- lib/crystal_gaze/crystal_ball.rb
+++ lib/crystal_gaze/crystal_ball.rb
@@ -5,6 +5,24 @@ module CrystalGaze
     def initialize(config = {})
       @config = config
     end
+
+    def predict(asking)
+      vision_type = asking.delete(:vision_of)
+
+      case vision_type
+      when /email/i then predict_email(asking)
+      end
+    end
+
+    private
+
+    def predict_email(asking)
+      spirit_world.vision_of(:email, asking)
+    end
+
+    def spirit_world
+      config[:spirit_world]
+    end
   end
 end
 
--- test/lib/crystal_gaze/crystal_ball_test.rb
+++ test/lib/crystal_gaze/crystal_ball_test.rb
@@ -5,11 +5,31 @@ module CrystalGaze
     subject { CrystalBall.new(config) }
 
     let(:config) do
-      { :samples => { :emails => { "Steve Jobs" => "s.j@apple.com" } } }
+      {
+        :spirit_world => spirit_world,
+        :spirits => { :emails => { "Steve Jobs" => "s.j@apple.com" } }
+      }
     end
 
-    it "exposes its configuration" do
-      assert_equal(config, subject.config)
+    let(:spirit_world) { MiniTest::Mock.new }
+
+    describe "#config" do
+      it "exposes its configuration" do
+        assert_equal(config, subject.config)
+      end
+    end
+
+    describe "#predict" do
+      let(:asking) { { :vision_of => "email", :name => name, :domain => domain } }
+      let(:name) { "Steve Wozniak" }
+      let(:domain) { "apple.com" }
+
+      it "enters the spirit world for a vision" do
+        spirit_world.expect(:vision_of, nil, [:email, :name => name, :domain => domain])
+        subject.predict(asking)
+
+        assert spirit_world.verify
+      end
     end
   end
 end
