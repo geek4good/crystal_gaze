66d14d51bd923edf0eb2060857d5735c2fd03455
Lucas Mbiwe <lucas@geek4good.com>
2014-10-01 21:36:10 +0200

    Outline the first test

---
+++ test/bin/predict_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+describe "when executing bin/predict" do
+  # given some sample data
+  # when I run `predict "Steve Wozniak" "apple.com"`
+  # then I should see the following output:
+  # ...
+end
