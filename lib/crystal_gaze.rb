5d693c38620430716958794557c0f24a6adb529a
Lucas Mbiwe <lucas@geek4good.com>
2014-10-02 14:56:29 +0200

    Start setting the mood for having visions
    * tune the crystal ball in the back-office instead of in front of the client
      (move the set-up code out of the executable)
    * prepare for more elaborate setup
      (by doing the above everything is set up to add more configuration)

--- lib/crystal_gaze.rb
+++ lib/crystal_gaze.rb
@@ -3,5 +3,14 @@ require "crystal_gaze/crystal_ball"
 require "crystal_gaze/spirit_world"
 
 module CrystalGaze
-  # Your code goes here...
+  def self.set_the_mood
+    @crystal_ball ||= CrystalBall.new(:spirit_world => CrystalGaze::SpiritWorld)
+  end
+
+  def self.consult_crystal_ball(asking)
+    set_the_mood
+
+    type, name, domain = asking
+    @crystal_ball.predict(:vision_of => type, :name => name, :domain => domain)
+  end
 end
--- test/bin/predict_test.rb
+++ test/bin/predict_test.rb
@@ -3,14 +3,6 @@ require "open3"
 
 describe "when executing bin/predict" do
   # given some sample data
-  samples = {
-    "John Ferguson" => "john.ferguson@alphasights.com",
-    "Damon Aw" => "damon.aw@alphasights.com",
-    "Linda Li" => "linda.li@alphasights.com",
-    "Larry Page" => "larry.p@google.com",
-    "Sergey Brin" => "s.brin@google.com",
-    "Steve Jobs" => "s.j@apple.com"
-  }
 
   it "predicts somebody's email address given their name and a domain" do
     input = %w(email Steve\ Wozniak apple.com)
