f67fac172e125413c28335a6ae2c2a5ff034acc0
Lucas Mbiwe <lucas@geek4good.com>
2014-10-03 11:10:33 +0100

    Add more forms of manifestation
    * add FirstNameDotLastName (steve.jobs)
    * add FirstNameDotLastInitial (steve.j)
    * add FirstInitialDotLastName (s.jobs)

--- lib/crystal_gaze/email_manifestation.rb
+++ lib/crystal_gaze/email_manifestation.rb
@@ -1,5 +1,38 @@
 module CrystalGaze
   class EmailManifestation
+    FirstNameDotLastName = Struct.new(:regex) do
+      def match?(gestalt)
+        regex =~ gestalt
+      end
+
+      def present(name, domain)
+        firstname, lastname = name.downcase.split
+        "#{firstname}.#{lastname}@#{domain}"
+      end
+    end
+
+    FirstNameDotLastInitial = Struct.new(:regex) do
+      def match?(gestalt)
+        regex =~ gestalt
+      end
+
+      def present(name, domain)
+        firstname, lastname = name.downcase.split
+        "#{firstname}.#{lastname.chr}@#{domain}"
+      end
+    end
+
+    FirstInitialDotLastName = Struct.new(:regex) do
+      def match?(gestalt)
+        regex =~ gestalt
+      end
+
+      def present(name, domain)
+        firstname, lastname = name.downcase.split
+        "#{firstname.chr}.#{lastname}@#{domain}"
+      end
+    end
+
     FirstInitialDotLastInitial = Struct.new(:regex) do
       def match?(gestalt)
         regex =~ gestalt
@@ -19,7 +52,12 @@ module CrystalGaze
       end
 
       def manifestations
-        @manifestations ||= [FirstInitialDotLastInitial.new(/\A\w\.\w\z/)]
+        @manifestations ||= [
+          FirstInitialDotLastInitial.new(/\A\w\.\w\z/),
+          FirstInitialDotLastName.new(/\A\w\.\w+\z/),
+          FirstNameDotLastInitial.new(/\A\w+\.\w\z/),
+          FirstNameDotLastName.new(/\A\w+\.\w+\z/)
+        ]
       end
     end
   end
--- test/lib/crystal_gaze/email_manifestation_test.rb
+++ test/lib/crystal_gaze/email_manifestation_test.rb
@@ -3,8 +3,17 @@ require "test_helper"
 module CrystalGaze
   describe EmailManifestation do
     describe ".of" do
-      it "instanciates a manifestation of a matching gestalt" do
+      it "instanciates a matching manifestation for 's.j'" do
         assert_instance_of(EmailManifestation::FirstInitialDotLastInitial, EmailManifestation.of("s.j"))
+      end
+      it "instanciates a matching manifestation for 'steve.j'" do
+        assert_instance_of(EmailManifestation::FirstNameDotLastInitial, EmailManifestation.of("steve.j"))
+      end
+      it "instanciates a matching manifestation for 's.jobs'" do
+        assert_instance_of(EmailManifestation::FirstInitialDotLastName, EmailManifestation.of("s.jobs"))
+      end
+      it "instanciates a matching manifestation for 'steve.jobs'" do
+        assert_instance_of(EmailManifestation::FirstNameDotLastName, EmailManifestation.of("steve.jobs"))
       end
     end
   end
